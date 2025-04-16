11dd5aec7ee6fea056d0a7f25553b02bab8c6810
piyushpawar19 <piyushpawar193@gmail.com>
2025-04-16 14:09:10 +0530

    install morgan

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-app.use(morgan("dev"));
+app.use(morgan());
 
 // // Routes
 // app.use('/api/auth', authRoutes);
